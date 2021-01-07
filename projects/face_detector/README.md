a3822c68c8e5c4d97b40b4fbb6ede19b94f273a4
Abhijit Roy <dalvath3700@gmail.com>
2021-01-07 22:29:02 +0530

    face detection complete in static images

--- projects/face_detector/README.md
+++ projects/face_detector/README.md
@@ -8,5 +8,51 @@
 	- convert to grayscale
 	- train into the algorithm
 
+## Demo
+* Detect single face in "photo with multiple faces"
+<p align="center">
+  <img src="data/1_face_detect.png" alt="" width="" height="">
+</p>
+	
+	- code
+```py
+(x, y, w, h) = face_coordinates[0]
+cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+```
+* Detect multiple faces in "photo with multiple faces"
+<p align="center">
+  <img src="data/multiple_faces_detect.png" alt="" width="" height="">
+</p>
+	
+	- code
+```py
+# detect multiple faces
+# for i in range(len(face_coordinates)):
+# 	x, y, w, h = face_coordinates[i];
+# 	cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# OR
+
+for x, y, w, h in face_coordinates:
+	cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+```
+
+* Make different colors of rectangles based on the faces
+<p align="center">
+  <img src="data/multiple_faces_detect_diff_color_boxes.png" alt="" width="" height="">
+</p>
+	
+	- code
+```py
+for x, y, w, h in face_coordinates:
+	cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)  # random color
+```
+	- only bright color
+```py
+for x, y, w, h in face_coordinates:
+	cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(128, 256), randrange(128, 256), randrange(128, 256)), 2)  # random color
+```
+
+
 ## References
 * [Python Artificial Intelligence Tutorial - AI Full Course for Beginners in 9 Hours [2021]](https://www.youtube.com/watch?v=XIrOM9oP3pA)
--- projects/face_detector/face_detector.py
+++ projects/face_detector/face_detector.py
@@ -1,11 +1,12 @@
 import cv2
+from random import randrange
 
 # load some pre-trained data on face frontals from opencv (haar cascade algorithm)
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # img = cv2.imread('data/RDJ.jpg')		# read image
-# img = cv2.imread('data/my_pic.jpg')		# read image
-img = cv2.imread('data/my_pic_2.jpg')		# read image
+# img = cv2.imread('data/my_pic_2.jpg')		# read image
+img = cv2.imread('data/2_faces.png')		# read image
 
 # Convert to grayscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -17,8 +18,27 @@ face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
 # Draw rectangles around the faces
 # params: image, top left, bottom right, Green, thickness
+# ----------------------------------------------------
+# detect single face
 (x, y, w, h) = face_coordinates[0]
 cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+# ----------------------------------------------------
+# detect multiple faces
+'''
+for i in range(len(face_coordinates)):
+	x, y, w, h = face_coordinates[i];
+	cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)   # Green color
+	# randomly decide the color of rectangle for faces
+	cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
+'''
+
+# OR
+
+for x, y, w, h in face_coordinates:
+	cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)		# make the face green color
+	# randomly decide the color of rectangle for faces
+	cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
+
 
 # cv2.imshow("Face Detector", grayscaled_img)		# show image
 cv2.imshow("Face Detector", img)		# show image
